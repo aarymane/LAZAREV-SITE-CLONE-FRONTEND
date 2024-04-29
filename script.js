013abde72790c57b7b1cfb3763718c51ab6fbf6b
Aaryesh_Mane <121445797+aarymane@users.noreply.github.com>
2024-04-29 09:06:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,109 +1,114 @@
-function navAnimation(){var nav=document.querySelector("nav")
-nav.addEventListener("mouseenter", function(){
-  let tl = gsap.timeline()
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
 
-        tl.to("#nav-bottom", {
-            height: "21vh",
-            duration: 0.5
-        })
-        tl.to(".nav-part2 h5", {
-            display: "block",
-            duration: 0.1
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
 
-        })
-        tl.to(".nav-part2 h5 span", {
-            y:0,
-            //duration:0.5,
-            stagger:{
-                amount:0.5,
-            }
-            
-        })
-    })
-nav.addEventListener("mouseleave", function(){
-  let tl = gsap.timeline()
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
+});
 
-        tl.to(".nav-part2 h5 span", {
-            y:50,
-            //duration:0.5,
-            stagger:{
-                amount:0.2,
-            }   
-        })
-        tl.to(".nav-part2 h5", {
-            display: "none",
-            duration: 0.1
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  duration: 0.5,
+  height: "110px",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
 
-        })
-        tl.to("#nav-bottom", {
-            height: "0vh",
-            duration: 0.4
-        })
-    })
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers: true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
 
+gsap.from("#about-us img,#about-us-in", {
+  y: 90,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#about-us",
+    scroller: "body",
+    // markers:true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+gsap.from(".card", {
+  scale: 0.8,
+  // opacity:0,
+  duration: 1,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    // markers:false,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#page4 h1", {
+  y: 50,
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    // markers:true,
+    start: "top 75%",
+    end: "top 70%",
+    scrub: 3,
+  },
+});
 
-
-}
-
-  function page2animation() {var rightElems = document.querySelectorAll(".right-elem")
-
-    rightElems.forEach(function (elem) {
-        elem.addEventListener("mouseenter", function () {
-            gsap.to(elem.childNodes[3], {
-                opacity: 1,
-                scale: 1
-            })
-        })
-
-         
-
-        elem.addEventListener("mouseleave", function () {
-            gsap.to(elem.childNodes[3], {
-                opacity: 0,
-                scale: 0
-            })
-        })
-        elem.addEventListener("mousemove", function (dets) {
-
-            gsap.to(elem.childNodes[3], {
-                x: dets.x - elem.getBoundingClientRect().x - 120,
-                y: dets.y - elem.getBoundingClientRect().y - 460
-            })
-        })
-    })
-
-
-
-  }
-
-  function page3videoanimation (){
-    var page3center = document.querySelector('.page3-center')
-  var video = document.querySelector('#page3 video')
-
-  page3center.addEventListener("click", function () {
-    video.play()
-    gsap.to(video,{
-        transform:"scaleX(1) scaleY(1)",
-        opacity:1,
-        borderRadius:0
-  })
-
- }) 
- 
- video.addEventListener("click",function(){
-    video.pause()
-    gsap.to(video,{
-        transform:"scaleX(0.7) scaleY(0)",
-        opacity:0,
-        borderRadius:"30px"
-  })
-
- })
-
-
-  }
-
-  
-//navAnimation();
-//page2animation();
-//page3videoanimation();
+// Thanks itna aage tak aane ke liye lekin pura code utha ke copy paste karne ki jagah khud ek baar banane ka try karna, kuch naya seekhne ko milega!
